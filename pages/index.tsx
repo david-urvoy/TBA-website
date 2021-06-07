2335b57f79bb6c9cc22691844954456a0e8bef7e
David Urvoy <david.urvoy.pro@gmail.com>
2021-06-07 16:04:18 +0000

    feat: sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,45 @@
+import axios from 'axios'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {useRouter} from 'next/router'
+import React, {useEffect, useState} from 'react'
+import MiniDrawer from '../src/components/sidebar/sidebar'
+import {login, logout} from '../src/oauth/auth'
+const tokenUrl = 'https://discord.com/api/users/@me'
 
 export default function Home() {
+	const [connectedUser, setConnectedUser] = useState('')
+
+	useEffect(() => {
+		const fragment = new URLSearchParams(window.location.hash.slice(1))
+		const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')]
+
+		if (!!accessToken) {
+			axios.get(tokenUrl, {headers: {authorization: `${tokenType} ${accessToken}`}})
+				.then(({data: {username}}) => setConnectedUser(`${username}`))
+				.catch(console.error)
+		}
+	})
+
+	const router = useRouter()
+
 	return (
-		<div className={styles.main}>
-			<div className={styles.container}>
+		<div className="main">
+			<div className="container">
+			</div>
+			<div>
 				<Head>
 					<title>Brain Academy</title>
 					<meta name="description" content="Brain Academy" />
 					<link rel="icon" href="/brain-academy-logo.ico" />
 				</Head>
+				<MiniDrawer></MiniDrawer>
+				{!connectedUser ?
+					<button onClick={() => login(router)}>login</button>
+					:
+					<React.Fragment>Bonjour, {connectedUser}
+						<div><button onClick={() => logout(router, () => setConnectedUser(''))}>logout</button></div>
+					</React.Fragment>
+				}
 			</div>
 		</div>
 	)
