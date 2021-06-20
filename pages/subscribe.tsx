957f4a034b206ffcb5765ed3efc45d963f7685f3
David Urvoy <david.urvoy.pro@gmail.com>
2021-06-20 00:52:08 +0000

    feat: add subscribe page

---
+++ pages/subscribe.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Subsribe() {
+	return (
+		<div>
+			<iframe id="haWidget" src="https://www.helloasso.com/associations/the-brain-academy/adhesions/adhesion-the-brain-academy/widget-vignette-horizontale" style={{width:"800px", height: "400px", border: "none", zIndex: 5}}></iframe>
+		</div>
+	)
+}
