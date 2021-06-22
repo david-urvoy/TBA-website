efbc8fd2a21514d25316e4d3a6345906d19b91b2
David Urvoy <david.urvoy.pro@gmail.com>
2021-06-22 00:52:06 +0000

    style: enlarge sidebar icon and set cursor on mouse hover

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import ToolsMenu from './tools-menu/tools-menu'
 
 export default function Navbar(props: {toggleSidebar: () => void}) {
 	return <div className={styles.navbar}>
-		<HamburgerIcon w={6} h={6} color="white" onClick={props.toggleSidebar} />
+		<HamburgerIcon w={8} h={8} color="white" style={{cursor: "pointer"}} onClick={props.toggleSidebar} />
 		<ToolsMenu />
 	</div>
 }
