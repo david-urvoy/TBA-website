823de2c538ba2ee47eaf322c9fa56806b1dda7a9
David Urvoy <david.urvoy.pro@gmail.com>
2021-07-04 19:28:28 +0000

    style: improve sidebar accordion menu

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -15,7 +15,7 @@ import React from 'react'
 export default function Sidebar(props) {
 	const router = useRouter()
 	return (
-		<div>
+		<>
 			<Drawer placement="left" onClose={props.closeSidebar} isOpen={props.isOpen || false}>
 				<DrawerOverlay />
 				<DrawerContent>
@@ -24,35 +24,33 @@ export default function Sidebar(props) {
 					<DrawerBody>
 						<Accordion allowToggle allowMultiple>
 							<AccordionItem>
-								<h2>
-									<AccordionButton>
-										<Box flex="1" textAlign="left">
-											L'association
-										</Box>
-										<AccordionIcon />
-									</AccordionButton>
-								</h2>
-								<AccordionPanel pb={4}>
-									<AccordionItem>
+								<AccordionButton>
+									<Box flex="1" textAlign="left">
+										L'association
+									</Box>
+									<AccordionIcon />
+								</AccordionButton>
+								<AccordionPanel pb={0}>
+									<AccordionItem pl={4} paddingBlock={2}>
 										<Link href="https://www.helloasso.com/associations/the-brain-academy/adhesions/adhesion-the-brain-academy/">Adhésion</Link>
 									</AccordionItem>
-									<AccordionItem>
+									<AccordionItem pl={4} paddingBlock={2}>
 										<Link href="https://www.helloasso.com/associations/the-brain-academy/formulaires/1/widget">Faire un don</Link>
 									</AccordionItem>
 								</AccordionPanel>
 							</AccordionItem>
 
 							<AccordionItem>
-								<h2>
-									<AccordionButton>
-										<Box flex="1" textAlign="left">
-											Ressources
-										</Box>
-										<AccordionIcon />
-									</AccordionButton>
-								</h2>
-								<AccordionPanel pb={4}>
-									<Link href="https://brain-academy.fr/wiki/botc/">Wiki - Blood on the Clocktower</Link>
+								<AccordionButton>
+									<Box flex="1" textAlign="left">
+										Ressources
+									</Box>
+									<AccordionIcon />
+								</AccordionButton>
+								<AccordionPanel pb={0}>
+									<AccordionItem pl={4} paddingBlock={2}>
+										<Link href="https://brain-academy.fr/wiki/botc/">Wiki - Blood on the Clocktower</Link>
+									</AccordionItem>
 								</AccordionPanel>
 							</AccordionItem>
 						</Accordion>
@@ -60,6 +58,6 @@ export default function Sidebar(props) {
 				</DrawerContent>
 			</Drawer>
 			{props.children}
-		</div>
+		</>
 	)
 }
