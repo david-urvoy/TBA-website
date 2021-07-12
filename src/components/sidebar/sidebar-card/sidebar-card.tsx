0852f59922487b47d6db5a5bb3835e11c267e551
David Urvoy <david.urvoy.pro@gmail.com>
2021-07-12 11:55:58 +0000

    style: add sidebar user card

---
+++ src/components/sidebar/sidebar-card/sidebar-card.tsx
@@ -0,0 +1,17 @@
+import {Flex, useColorMode} from "@chakra-ui/react"
+import React, {useContext} from "react"
+import {ConnectedUserContext} from "../../../context/connected-user-context"
+import UserIcon from "../../user-icon"
+
+export default function SidebarCard() {
+	const {connectedUser} = useContext(ConnectedUserContext)
+	const {colorMode} = useColorMode()
+	return <Flex
+		rounded="lg" paddingInline={4} paddingBlock={2}
+		justify="space-between" align="center"
+		bg={colorMode === "light" ? "gray.300" : "gray.500"}
+	>
+		<Flex fontWeight="bold">{connectedUser?.username}</Flex>
+		<UserIcon />
+	</Flex>
+}
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,35 +1,31 @@
-import {Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Stack, Switch, useColorMode} from "@chakra-ui/react"
+import {Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Grid, GridItem, Stack, Switch, useColorMode} from "@chakra-ui/react"
 import React from "react"
 import theme from "../../../styles/theme"
 import navigationItems from "../../navigation-items"
-import UserIcon from "../user-icon"
+import SidebarCard from "./sidebar-card/sidebar-card"
 import SidebarItem from "./sidebar-item/sidebar-item"
 
 export default function Sidebar(props) {
 	const {colorMode, toggleColorMode} = useColorMode()
-	return (
-		<>
-			<Drawer placement="left" onClose={props.closeSidebar} isOpen={props.isOpen || false}>
-				<DrawerOverlay />
-				<DrawerContent padding={5}>
-					<DrawerBody>
-						<Stack>
-							{navigationItems.map(item => <SidebarItem key={item.title} item={item} closeSidebar={props.closeSidebar} />)}
-						</Stack>
-					</DrawerBody>
-					<Flex
-						bg={colorMode === "light" ? "gray.200" : "gray.600"}
-						padding={4}
-						align="center"
-						justify="space-between"
-						rounded="lg"
-					>
-						<Switch size={theme.media} onChange={toggleColorMode} />
-						<UserIcon />
-					</Flex>
-				</DrawerContent>
-			</Drawer>
-			{props.children}
-		</>
-	)
+	return <>
+		<Drawer placement="left" onClose={props.closeSidebar} isOpen={props.isOpen || false}>
+			<DrawerOverlay />
+			<DrawerContent padding={5}>
+				<DrawerBody>
+					<Stack>
+						{navigationItems.map(item => <SidebarItem key={item.title} item={item} closeSidebar={props.closeSidebar} />)}
+					</Stack>
+				</DrawerBody>
+				<Grid
+					bg={colorMode === "light" ? "gray.200" : "gray.600"}
+					padding={2}
+					alignItems="center"
+					rounded="lg" templateColumns="repeat(3, 1fr)">
+					<GridItem justifySelf="center"><Switch size={theme.media} onChange={toggleColorMode} /></GridItem>
+					<GridItem colSpan={2}><SidebarCard /></GridItem>
+				</Grid>
+			</DrawerContent>
+		</Drawer>
+		{props.children}
+	</>
 }
