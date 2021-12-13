cce979655e29c1c95d14fcdbd3e0b010d114bf04
David Urvoy <david.urvoy.pro@gmail.com>
2021-12-13 20:46:59 +0100

    config for app engine

--- pages/botc-herald.tsx
+++ pages/botc-herald.tsx
@@ -1,8 +1,7 @@
-import BotcHerald from "@brain-academy/botc-herald"
+//import BotcHerald from "@brain-academy/botc-herald"
 import React from "react"
 
 export default function Herald() {
 	return <div style={{height: "100%"}}>
-		<BotcHerald />
 	</div>
 }
