82ed0c5127af1e82dee208de7dec8adec1e184ca
David Urvoy <david.urvoy.pro@gmail.com>
2021-12-27 10:37:55 +0000

    port 8080

--- src/oauth/auth.ts
+++ src/oauth/auth.ts
@@ -1,11 +1,10 @@
 import {NextRouter} from 'next/router'
 
 const redirectUrl = "https://discord.com/api/oauth2/authorize?client_id=818238353345478676&redirect_uri=https%3A%2F%2Fbrain-academy.fr&response_type=token&scope=identify"
-const redirectUrlDev = "https://discord.com/api/oauth2/authorize?client_id=818238353345478676&redirect_uri=http%3A%2F%2Fbrain-academy.fr%3A3000&response_type=token&scope=identify"
-
+const redirectUrlDev = "https://discord.com/api/oauth2/authorize?client_id=818238353345478676&redirect_uri=http%3A%2F%2Fbrain-academy.fr%3A8080&response_type=token&scope=identify"
 
 export function login(router: NextRouter) {
-	return router.push(redirectUrlDev)
+	return router.push(redirectUrl)
 }
 
 export function logout(router: NextRouter, ...callback: (() => void)[]) {
