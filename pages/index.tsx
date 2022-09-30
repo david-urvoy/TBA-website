efb0c4e7fc12e9a99ae4c7723939811954af89a5
David Urvoy <david.urvoy.pro@gmail.com>
2022-10-01 00:25:58 +0200

    Add TBA plaquettes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
+import { Flex } from '@chakra-ui/react'
 import Head from 'next/head'
-import React from 'react'
-import styles from "./home.module.scss"
+import Image from 'next/image'
+import plaquetteRectoImg from '../public/images/plaquette-tba-recto.png'
+import plaquetteVersoImg from '../public/images/plaquette-tba-verso.png'
 
 export default function Home() {
 	return <>
 		<Head>
 			<title>Brain Academy</title>
-			<meta name="description" content="Brain Academy" />
-			<link rel="icon" href="/brain-academy-logo.ico" />
+			<meta name='description' content='Brain Academy' />
+			<link rel='icon' href='/brain-academy-logo.ico' />
 		</Head>
-		<div className={styles.container}>
-		</div>
+
+		{/* <div className={styles.container}>
+		</div> */}
+
+		<Flex alignItems='center' justifyContent='center' direction='column' >
+			<Image src={plaquetteRectoImg} quality={100} style={{ paddingBlock: '50px' }} />
+			<Image src={plaquetteVersoImg} quality={100} />
+		</Flex>
 	</>
 }
