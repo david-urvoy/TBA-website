9fa884b1d2933b2885d6caa5d29a9b77be64dc3c
David Urvoy <david.urvoy.pro@gmail.com>
2022-12-23 14:33:48 +0000

    fix: hydration link issue and set link to external

--- src/components/partner/PartnerCard.tsx
+++ src/components/partner/PartnerCard.tsx
@@ -1,37 +1,34 @@
 import {Card, CardBody, CardHeader, Center, Divider, Flex, Heading, Image, Link, StackDivider, VStack} from '@chakra-ui/react'
 import * as CSS from 'csstype'
-import NextLink from 'next/link'
 import PartnerCardLabel from './PartnerCardLabel'
 
 export default function PartnerCard({name, address, colorTheme = '', backgroundTheme = '', description = '', iconUrl, websiteUrl = '', iframeUrl}
 	: {name: string, address: string, colorTheme?: CSS.Property.Color, backgroundTheme?: CSS.Property.Color, description?: string, iconUrl: string, websiteUrl?: string, iframeUrl: string}) {
-	return <NextLink href={websiteUrl}>
-		<Card backgroundColor={backgroundTheme || null}>
-			<CardHeader>
-				<Center>
-					<Image src={iconUrl} loading='lazy' h='60px' pos='absolute' style={{left: '15px'}} />
-					<Heading color={colorTheme}>
-						{name}
-					</Heading>
-				</Center>
-			</CardHeader>
-			<Divider />
-			<CardBody>
-				<VStack direction='column' align='stretch' h='100%' divider={<StackDivider />} alignItems='space-evenly' spacing='20px'>
-					<VStack h='100%' justify='end'>
-						{!!description &&
-							<Center fontSize='30px'>{description}</Center>
-						}
-						<PartnerCardLabel name='Adresse'>{address}</PartnerCardLabel>
-						{!!websiteUrl &&
-							<PartnerCardLabel name='Site web'><Link href={websiteUrl}>{websiteUrl}</Link></PartnerCardLabel>
-						}
-					</VStack>
-					<Flex alignItems='end'>
-						<iframe src={iframeUrl} width='100%' height='350px' style={{border: '0'}} loading='lazy'></iframe>
-					</Flex>
+	return <Card backgroundColor={backgroundTheme || null}>
+		<CardHeader>
+			<Center>
+				<Image src={iconUrl} loading='lazy' h='60px' pos='absolute' style={{left: '15px'}} />
+				<Heading color={colorTheme}>
+					{name}
+				</Heading>
+			</Center>
+		</CardHeader>
+		<Divider />
+		<CardBody>
+			<VStack direction='column' align='stretch' h='100%' divider={<StackDivider />} alignItems='space-evenly' spacing='20px'>
+				<VStack h='100%' justify='end'>
+					{!!description &&
+						<Center fontSize='30px'>{description}</Center>
+					}
+					<PartnerCardLabel name='Adresse'>{address}</PartnerCardLabel>
+					{!!websiteUrl &&
+						<PartnerCardLabel name='Site web'><Link href={websiteUrl} isExternal={true}>{websiteUrl}</Link></PartnerCardLabel>
+					}
 				</VStack>
-			</CardBody>
-		</Card>
-	</NextLink>
+				<Flex alignItems='end'>
+					<iframe src={iframeUrl} width='100%' height='350px' style={{border: '0'}} loading='lazy'></iframe>
+				</Flex>
+			</VStack>
+		</CardBody>
+	</Card>
 }
